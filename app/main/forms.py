b9093bb98c2d3c4eb6414823a8ea8cdca37b1e7e
UmurerwaDivine <divinekagame@gmail.com>
2019-02-28 13:34:50 +0200

    edited readme

--- app/main/forms.py
+++ app/main/forms.py
@@ -11,7 +11,7 @@ class UpdateProfile(FlaskForm):
     bio = TextAreaField('tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
 class CommentForm(FlaskForm):
-      posted = DateTimeField('date and time', validators = [Required()])
+      posted = DateTimeField('date and time',format='%Y-%m-%d %H:%M:%S', validators = [Required()])
       description = TextAreaField('your comment ', validators=[Required()])
       submit = SubmitField('Submit')
    
